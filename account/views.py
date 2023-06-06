cfdae7f4a4f9fbec528fa75a6c50c8f6a4f98aeb
Adam Dekan <adam.dekan@gmail.com>
2023-06-06 17:04:24 +0200

    login, logout, dashboard basic

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -12,8 +13,8 @@ def user_login(request):
             cd = form.cleaned_data
             user = authenticate(
                 request,
-                username=cd("username"),
-                password=cd("password"),
+                username=cd["username"],
+                password=cd["password"],
             )
             if user is not None:
                 if user.is_active:
@@ -26,3 +27,8 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
